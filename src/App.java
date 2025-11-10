f27632e8b5dbe5ed699b8ad1541334890dc99159
Samuel <samuelruizmartin2412@gmail.com>
2025-11-10 22:24:32 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,50 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 0;
+        int posicion = 0;
+        int insertar = 0;
+
+        boolean correcto = false;
+        do {
+            num = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+            posicion = Integer.parseInt(System.console().readLine("Introduzca la posición donde quiere insertar: "));
+            insertar = Integer.parseInt(System.console().readLine("Introduzca el dígito que quiere insertar: "));
+
+            if (num <= 0)
+                System.out.println("Introduce un numero mayor que 0");
+            else
+                correcto = true;
+
+        } while (!correcto);
+
+        int aux = num * 10 + 1;
+        int invertido = 0;
+        int numDigito = 0;
+        while (aux > 0) {
+            int digito = aux % 10;
+            invertido = invertido * 10 + digito;
+            numDigito++;
+            aux /= 10;
+        }
+        numDigito--;
+        int parte1 = 0;
+        int parte2 = 0;
+        int numParte2 = 0;
+        for (int i = 1; i <= numDigito; i++) {
+
+            int digito = invertido % 10;
+
+            if (i < posicion) {
+                parte1 = parte1 * 10 + digito;
+            } else {
+                parte2 = parte2 * 10 + digito;
+                numParte2++;
+            }
+            invertido /= 10;
+        }
+        System.out.printf("Los números partidos son el %d y el %d.%n", parte1, parte2);
+
+        int entro = (parte1 * 10 + insertar) * (int)(Math.pow(10, numParte2)) + parte2;
+        System.out.printf("El número resultante es %d.", entro);
+    }
+}
